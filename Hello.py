b2ba7424a5c2f62e47a6c88c6953bca8e38bb18b
Dhaval Goyal <goyaldhaval024@gmail.com>
2026-05-16 17:14:33 +0530

    Add greeting program that interacts with user
    This program greets the user, asks for their name and job, and displays the information in both title case and uppercase.

---
+++ Hello.py
@@ -0,0 +1,13 @@
+# This is a simple Python program that greets the user and asks for their name and job.
+print (" Hello World ")
+user_name = input("What is your name? ")
+user_name = user_name.strip().title() # Remove any leading or trailing whitespace and convert the name to title case (first letter of each word capitalized)
+#user_name = input("What is your name? ").strip().title() This can be also used instead of 2 lines we can do it in a single line
+print (f"Hello {user_name}!")
+print ("Welcome to Python programming!")    
+user_job = input("What is your job? ")
+user_job = user_job.strip().title() # Remove any leading or trailing whitespace and convert the job to title case (first letter of each word capitalized)
+#user_job = input("What is your job? ").title().split() This can be also used instead of 2 lines we can do it in a single line
+print (f"Wow! {user_job} is a great job!")
+print (f"UPPERCASE Name: {user_name.upper()} ")
+print (f"UPPERCASE Job: {user_job.upper()} ")
