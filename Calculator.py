25ea0810b33010e30cba9f73c683f6b732a65ffc
Dhaval Goyal <goyaldhaval024@gmail.com>
2026-05-18 18:36:26 +0530

    Add basic calculator functionality in Calculator.py
    Implement basic arithmetic operations and display results.

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+x = float(input("Enter a number: "))
+y = float(input("Enter another number: "))
+z = x + y
+a = x - y
+b = x * y   
+c = x / y
+d = x % y
+print(f"The sum is:{z:.2f}") # This will print the sum of x and y with 2 decimal places
+print(f"The difference is:{a:.2f}")
+print(f"The product is:{b:.2f}")
+print(f"The quotient is:{c:.2f}")
+print(f"The remainder is:{d:.2f}")
