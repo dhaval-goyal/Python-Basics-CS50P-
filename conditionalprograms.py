fed261756266c54893b7a972fc84e5e2e98c1e17
Dhaval Goyal <goyaldhaval024@gmail.com>
2026-05-20 22:09:29 +0530

    Add comparison logic for two user-input numbers

--- conditionalprograms.py
+++ conditionalprograms.py
@@ -1,1 +1,10 @@
+x = float(input("Select any number 1:"))
+y = float(input("Select any number 2:"))
+if x > y:
+    print(f"{x} is greater than {y}")
 
+elif x == y:
+    print(f"{x} is equal to {y}")
+
+else:
+    print(f"{y} is greater than {x}")
