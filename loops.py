5e5d4748f5c15b4cf3b351e4539de1087a71393b
Dhaval Goyal <goyaldhaval024@gmail.com>
2026-06-01 14:49:58 +0530

    Implement while loop for repeated printing
    Add a while loop to print 'Hello World' three times.

---
+++ loops.py
@@ -0,0 +1,6 @@
+#loops
+i=0
+while i<3:
+    print("Hello World")
+    i+=1
+    #this will print "Hello World" 3 times because the loop will run until i is less than 3 and i is incremented by 1 in each iteration
